6eda108011f886b80050618c76216dd96adf3b1e
Taro Aoki <aizu.s1230022@gmail.com>
2016-12-26 00:22:22 +0900

    [ADD] Some commands template

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"os"
+
+	"github.com/urfave/cli"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "tmp-cli"
+	app.Version = "0.1.0"
+	app.Usage = "Easily manage your all temporary directories"
+	app.Commands = commands
+	app.Run(os.Args)
+}
