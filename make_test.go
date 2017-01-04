3f1d71f5f1732083690674359424c695595b2f6a
Taro Aoki <aizu.s1230022@gmail.com>
2017-01-05 00:47:24 +0900

    [MOD] Added clean up when test "tmp make" command

--- make_test.go
+++ make_test.go
@@ -2,26 +2,36 @@ package main
 
 import (
 	"bytes"
+	"io/ioutil"
 	"os"
+	"os/user"
+	"path/filepath"
 	"strings"
 	"testing"
 
 	"github.com/urfave/cli"
 )
 
-func TestParseArguments(t *testing.T) {
+func TestMakeDir(t *testing.T) {
 	outStream, errStream := new(bytes.Buffer), new(bytes.Buffer)
 
 	tests := []struct {
 		command string
+		path    string
+		name    string
 		success bool
 	}{
-		{"tmp make", true},
+		{
+			command: "tmp make",
+			success: true,
+		},
 	}
 
 	app := cli.NewApp()
 	app.Writer, app.ErrWriter = outStream, errStream
 	app.Commands = commands
+
+	user, _ := user.Current()
 
 	for _, test := range tests {
 		args := strings.Split(test.command, " ")
@@ -29,6 +39,24 @@ func TestParseArguments(t *testing.T) {
 			t.Fatal(err)
 		}
 
-		os.Remove("./tmp/")
+		if test.name == "" {
+			test.name = "tmp"
+		}
+
+		os.Remove(filepath.Join(test.path, test.name))
+
+		// Tear down
+		configFilePath := filepath.Join(user.HomeDir, dirName, configFileName)
+		contentBytes, err := ioutil.ReadFile(configFilePath)
+		if err != nil {
+			t.Fatal(err)
+		}
+		content := string(contentBytes)
+		splitted := strings.Split(content, "\n")
+
+		if content != "" {
+			// Remove item from ~/.tmp-cli/config
+			ioutil.WriteFile(configFilePath, []byte(strings.Join(splitted[:len(splitted)-2], "\n")), 0644)
+		}
 	}
 }
