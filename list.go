6ef822a3edc558da6c4d782a59eafa86b68104da
Taro Aoki <aizu.s1230022@gmail.com>
2017-01-09 17:16:28 +0900

    [MOD] Remove unnecessary code

--- list.go
+++ list.go
@@ -7,32 +7,12 @@ import (
 )
 
 func list(c *cli.Context) error {
-	// user, _ := user.Current()
-	//
-	// f, err := os.Open(filepath.Join(user.HomeDir, dirName, configFileName))
-	// if err != nil {
-	// 	return fmt.Errorf("cannot open tmp config file: %s", err.Error())
-	// }
-	//
-	// list := ""
-	// index := 0
-	// s := bufio.NewScanner(f)
-	// for s.Scan() {
-	// 	line := s.Text()
-	// 	if c.BoolT("number") {
-	// 		line = fmt.Sprintf("[%d] ", index) + line
-	// 	}
-	// 	line += "\n"
-	//
-	// 	list += line
-	//
-	// 	index++
-	// }
-	//
-	// fmt.Println(list)
-
+	index := 0
 	list := PathList()
 	for _, item := range list {
+		if c.BoolT("number") {
+			fmt.Printf("[%d] %s", index, item)
+		}
 		fmt.Println(item)
 	}
 
